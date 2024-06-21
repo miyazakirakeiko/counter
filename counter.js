708e814f0d015a6db674f6ce3faf4e2918a240de
宮崎良太 <>
2024-06-22 00:01:54 +0900

    counter

--- counter.js
+++ counter.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+(() => {
+  const $counter = document.getElementById("js-counter");
+
+  const clickHandler = (e) => {
+    const $targetButton = e.currentTarget;
+    let currentCount = perseInt($counter.textContent);
+    if ($targetButton.textContent === "+") {
+      $counter.textContent = currentCount + 1;
+    } else {
+      $counter.textContent = currentCount - 1;
+    }
+  };
+
+  for (
+    let index = 0;
+    index < document.getElementsByClassName("js-button").length;
+    index++
+  ) {
+    document
+      .getElementsByClassName("js-button")
+      [index].addEventListener("click", (e) => clickHandler(e));
+  }
+})();
--- reset.js
+++ reset.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+(() => {
+  const $counter = document.getElementById("js-counter");
+
+  const clickHandler = () => {
+    $counter.textContent = 0;
+  };
+
+  document
+    .getElementById("js-reset-button")
+    .addEventListener("click", clickHandler);
+})();
